b532015864304473011ea0b8dc260abd5ef6f24e
Isabella <bellasofiamartins@gmail.com>
2023-04-01 03:53:42 -0300

    the ordinary web page

---
+++ script.js
@@ -0,0 +1,113 @@
+function btn1on(){
+    var changeBtn = document.getElementById('btn1');
+    document.getElementById('txt1').style.visibility = "visible";
+    document.getElementById('price1').style.visibility = "visible";
+     changeBtn.style.backgroundColor = 'black';
+     changeBtn.style.color = "white";
+}
+
+function btn1out(){
+    var changeBtn = document.getElementById('btn1');
+    changeBtn.style.backgroundColor = 'white';
+    changeBtn.style.color = "black";
+    txt1.style.visibility = 'hidden';
+    price1.style.visibility = "hidden";
+}
+
+function onlybtn1on(){
+    var btnover = document.getElementById('btn1');
+        btnover.style.backgroundColor = 'black';
+        btnover.style.color = "white";  
+}
+
+function onlybtn1out(){
+    btnover = document.getElementById('btn1');
+    btnover.style.backgroundColor = 'white';
+    btnover.style.color = "black";  
+}
+function btn2on(){
+    var changeBtn = document.getElementById('btn2');
+    document.getElementById('txt2').style.visibility = "visible";
+    document.getElementById('price2').style.visibility = "visible";
+     changeBtn.style.backgroundColor = 'black';
+     changeBtn.style.color = "white";
+}
+function btn2out(){
+    var changeBtn = document.getElementById('btn2');
+    changeBtn.style.backgroundColor = 'white';
+    changeBtn.style.color = "black";
+    txt2.style.visibility = 'hidden';
+    price2.style.visibility = "hidden";
+}
+function onlybtn2on(){
+    var btnover = document.getElementById('btn2');
+        btnover.style.backgroundColor = 'black';
+        btnover.style.color = "white";  
+}
+
+function onlybtn2out(){
+    btnover = document.getElementById('btn2');
+    btnover.style.backgroundColor = 'white';
+    btnover.style.color = "black";  
+}
+function btn3on(){
+    var changeBtn = document.getElementById('btn3');
+    document.getElementById('txt3').style.visibility = "visible";
+    document.getElementById('price3').style.visibility ="visible";
+    changeBtn.style.backgroundColor = "black";
+    changeBtn.style.color = "white";
+
+}
+function btn3out(){
+    var changeBtn = document.getElementById('btn3');
+    changeBtn.style.backgroundColor = 'white';
+    changeBtn.style.color = "black";
+    txt3.style.visibility = 'hidden';
+    price3.style.visibility = "hidden";
+}
+function onlybtn3on(){
+    var btnover = document.getElementById('btn3');
+        btnover.style.backgroundColor = 'black';
+        btnover.style.color = "white";  
+}
+
+function onlybtn3out(){
+    btnover = document.getElementById('btn3');
+    btnover.style.backgroundColor = 'white';
+    btnover.style.color = "black";  
+}
+
+function showTxtImgCleanser(){
+    document.getElementById('txtImgCleanser').style.visibility="visible";
+    document.getElementById('ImgCleanser').style.transition = "all 1s";
+}
+function legend1(){
+    document.getElementById('legend1').style.visibility = "visible";
+}
+function legend1out(){
+    document.getElementById('legend1').style.visibility = "hidden"
+}
+function legend2(){
+    document.getElementById('legend2').style.visibility = "visible";
+}
+function legend2out(){
+    document.getElementById('legend2').style.visibility = "hidden"
+}
+function legend3(){
+    document.getElementById('legend3').style.visibility = "visible";
+}
+function legend3out(){
+    document.getElementById('legend3').style.visibility = "hidden";
+}
+function legend4(){
+    document.getElementById('legend4').style.visibility = "visible";
+}
+function legend4out(){
+    document.getElementById('legend4').style.visibility = "hidden";
+}
+function legend5(){
+    document.getElementById('legend5').style.visibility = "visible";
+}
+function legend5out(){
+    document.getElementById('legend5').style.visibility = "hidden";
+}
